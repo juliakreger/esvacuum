8cf8f44bea40b08c9b26c7458d0b056d06c0b7bb
Julia Kreger <jkreger@gmail.com>
2014-01-01 10:12:48 -0500

    Adding flag to only copy a single index, minor clean-ups, and futher rubificiation of some of the output logic... helps if you actually add the file to be committed.

--- lib/esvacuum.rb
+++ lib/esvacuum.rb
@@ -18,6 +18,7 @@ module Esvacuum
   # @option arguments [Boolean] :verbose Verbose output.  (default: false)
   # @option arguments [String] :newindexname A string if defined that is used to replace the target document index.
   # @option arguments [String] :newtypename A string if defined that is used to replace the target document type.
+  # @option arguments [String] :sourceindex A string defining a single index to read from if it exists.
   #
 
   def self.execute( arguments={} )
@@ -31,7 +32,6 @@ module Esvacuum
       arguments[:size] = 100
     end
     if arguments[:verbose].nil?
-      #TODO change to false
       arguments[:verbose] = false 
     end
 
@@ -45,8 +45,12 @@ module Esvacuum
     rescue 
       raise "Error connecting to #{arguments[:destination]}"  
     end
-
-    indexqueryresponse = sourceClient.indices.get_mapping
+    if !arguments[:sourceindex].nil?
+      indexqueryresponse = Hash.new
+      indexqueryresponse = { arguments[:sourceindex] => "" }
+    else
+      indexqueryresponse = sourceClient.indices.get_mapping
+    end
     indexqueryresponse.each_key do | indexname |
       beginning_time = Time.now
       begin
@@ -59,15 +63,13 @@ module Esvacuum
         raise "Error Searching Index: #{indexname}"
         next
       end
-      if arguments[:verbose] == true
-        puts "Processing Index: #{indexname}"
-      end
+
+      puts "Processing Index: #{indexname}" if arguments[:verbose] == true
+
       recordcount = 1
       sourceSearchResponse = sourceClient.scroll scroll: '5m', scroll_id: sourceSearchResponse['_scroll_id']
 
-      if arguments[:verbose] == true
-        puts "Index #{indexname} contains #{sourceSearchResponse['hits']['total']} items."
-      end
+      puts "Index #{indexname} contains #{sourceSearchResponse['hits']['total']} items." if arguments[:verbose] == true
 
       until sourceSearchResponse['hits']['hits'].size == 0 do
 
@@ -97,9 +99,7 @@ module Esvacuum
 
     end
 
-    if arguments[:verbose] == true
-      puts "Completed"
-    end 
+    puts "Completed" if arguments[:verbose] == true
 
   end
 end
